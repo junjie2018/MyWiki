92a0da4fda97723536d9f474bded0a1dc9d941f0
小桀 <wujj@corp.4dstc.com>
2022-04-20 14:10:49 +0800

    add new code

--- src/main/java/junjie/fun/mywiki/service/EBookService.java
+++ src/main/java/junjie/fun/mywiki/service/EBookService.java
@@ -2,11 +2,9 @@ package junjie.fun.mywiki.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
 import junjie.fun.mywiki.entity.EBook;
-import junjie.fun.mywiki.request.ebook.CreateEBookRequest;
-import junjie.fun.mywiki.request.ebook.PageEBookRequest;
-import junjie.fun.mywiki.request.ebook.UpdateEBookRequest;
-import junjie.fun.mywiki.response.PageData;
-import junjie.fun.mywiki.response.data.EBookData;
+import junjie.fun.mywiki.request.ebook.*;
+import junjie.fun.mywiki.response.*;
+import junjie.fun.mywiki.response.data.*;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -16,11 +14,12 @@ public interface EBookService extends IService<EBook> {
 
     Long createEBook(CreateEBookRequest request);
 
+    void deleteEBooks(List<Long> eBookIds);
+
     Long updateEBook(UpdateEBookRequest request);
 
     PageData<EBookData> pageEBook(PageEBookRequest request);
 
     List<EBookData> queryEBooks(List<Long> ebookIds);
 
-    void deleteEBooks(List<Long> eBookIds);
 }
